3f1a6c40ce5a198bd7829cdff96989f7df3a1a15
Tucker Chaney <tucker.chaney@yahoo.com>
2025-01-23 15:01:50 -0600

    exercises 1-5

--- 06-objects-dom/main.js
+++ 06-objects-dom/main.js
@@ -11,18 +11,64 @@
 // };
 // person.greet("");
 
-const el = document.querySelector("#my-unordered-list");
-console.log(el);
+// const el = document.querySelector("#my-unordered-list");
+// console.log(el);
 
-const btn = document.querySelector("#btn");
-console.log(btn);
+// const btn = document.querySelector("#btn");
+// console.log(btn);
 
-btn.addEventListener("click", btnhandler);
+// btn.addEventListener("click", btnhandler);
 
-function btnhandler() {
+// function btnhandler() {
+//   const listItem = document.createElement("li");
+//   listItem.innerHTML = `<li/> List Item 1 </li>
+// <li/> List Item 2 </li>
+// <li/> List Item 3 </li>`;
+//   el.appendChild(listItem);
+// }
+const car = {
+  make: "Ford",
+  model: "Taurus",
+  year: "1993",
+};
+console.log("I drive a", car.year, car.make, car.model);
+
+const student = {
+  name: "Tucker",
+  age: 25,
+  grades: [10, 10, 10, 0, 0],
+  averageGrade: function () {
+    const total = this.grades.reduce(function (acc, grade) {
+      return acc + grade;
+    }, 0);
+    return total / this.grades.length;
+  },
+};
+console.log(
+  `My student ${student.name}, age ${
+    student.age
+  }, has an average grade of:${student.averageGrade()}`
+);
+
+const myBtn = document.getElementById("myBtn");
+myBtn.addEventListener("click", function () {
+  myBtn.style.backgroundColor = "lightblue";
+  console.log("I clicked");
+});
+
+const textBtn = document.getElementById("textBtn");
+const myParagraph = document.getElementById("myParagraph");
+
+textBtn.addEventListener("click", function () {
+  myParagraph.textContent = "new text!";
+});
+
+const myList = document.getElementById("myList");
+const btnList = document.getElementById("btnList");
+
+function addToList() {
   const listItem = document.createElement("li");
-  listItem.innerHTML = `<li/> List Item 1 </li>
-<li/> List Item 2 </li> 
-<li/> List Item 3 </li>`;
-  el.appendChild(listItem);
+  listItem.textContent = "New List Item";
+  myList.appendChild(listItem);
 }
+btnList.addEventListener("click", addToList);
